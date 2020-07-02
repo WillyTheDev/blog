7d237cb44bc5a44a0014da236605d36108dbda4f
William <melting.dev@gmail.com>
2020-07-02 09:15:49 +0200

    modify gitignore

--- app.js
+++ app.js
@@ -93,6 +93,11 @@ app.post("/compose", async (req, rep) => {
   rep.redirect("/");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port : 3000");
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, () => {
+  console.log("Server has started successfully !");
 });
